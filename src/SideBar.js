1359f21eac0792f7f494910885bc2ed937a4beca
Heba <hebahiary+github@gmail.com>
2023-05-03 11:48:45 -0700

    initial upload

---
+++ src/SideBar.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function SideBar() {
+  return (
+    <div className="">
+      <i>A</i>
+      <i>B</i>
+      <i>C</i>
+      <i>D</i>
+      <i>E</i>
+    </div>
+  );
+}
+
+export default SideBar;
